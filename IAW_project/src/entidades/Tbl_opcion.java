a72ad56fd638aefa71016e31b69bf8a910177171
Leonardo <leonardoorozco69@gmail.com>
2019-09-26 22:07:47 -0600

    Entidades seguridad

---
+++ IAW_project/src/entidades/Tbl_opcion.java
@@ -0,0 +1,37 @@
+package entidades;
+
+public class Tbl_opcion {
+	
+	private int id_rol;
+	private String opcion;
+	private String opcion_des;
+	private int estado;
+	
+	public int getId_rol() {
+		return id_rol;
+	}
+	public void setId_rol(int id_rol) {
+		this.id_rol = id_rol;
+	}
+	public String getOpcion() {
+		return opcion;
+	}
+	public void setOpcion(String opcion) {
+		this.opcion = opcion;
+	}
+	public String getOpcion_des() {
+		return opcion_des;
+	}
+	public void setOpcion_des(String opcion_des) {
+		this.opcion_des = opcion_des;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	
+	
+
+}
